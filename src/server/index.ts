880d3d238022394098d19df190567ba687053def
92Mohammad <m.e89571828@gmail.com>
2024-05-21 22:02:47 +0530

    all file deleted to update with new one

--- src/server/index.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const app = express();
-
-app.get('/', (req, res) => {
-    res.send( {message: "hello check hello"})
-})
-
-app.listen(3000, () => console.log("server is running or port " + 3000));
