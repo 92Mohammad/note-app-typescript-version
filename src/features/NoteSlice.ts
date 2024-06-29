1ee714ca7918fb4d2a75a00f7054f182bc68c8fc
92Mohammad <m.e89571828@gmail.com>
2024-06-29 11:04:41 +0530

    fixes getNote and create Note method

--- src/features/NoteSlice.ts
+++ src/features/NoteSlice.ts
@@ -1,106 +1,107 @@
-import { createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit'
-import { NoteType, BASE_URL, createNoteProps } from '../utils'
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { NoteType, BASE_URL, createNoteProps } from "../utils";
 
-export interface noteState {
-    notes: NoteType[];
-    noteTitle: string;
+export interface NoteState {
+  notes: NoteType[];
+  noteTitle: string;
+  errors: any;
 }
-const initialState: noteState = {
-    notes: [],
-    noteTitle: ""
-}
+const initialState: NoteState = {
+  notes: [],
+  noteTitle: "",
+  errors: null,
+};
 
-export const fetchAllNotes = createAsyncThunk('note/getAllNotes', async() => {
-    try{
-        const res = await fetch(`${BASE_URL}/note/getAllNotes`, {
-          method: 'GET',
-          headers:{
+export const fetchAllNotes = createAsyncThunk(
+  "note/getAllNotes",
+  async (_, thunkAPI) => {
+    try {
+      const res = await fetch(`${BASE_URL}/note/getAllNotes`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          authorization: localStorage.getItem("authToken")!,
+        },
+      });
+      return await res.json();
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+export const createNewNote = createAsyncThunk(
+  "note/createNewNote",
+  async (
+    { noteTitle, setNoteTitle, addNewNotes }: createNoteProps,
+    thunkAPI
+  ) => {
+    try {
+      if (noteTitle !== "") {
+        const createNoteParameter = {
+          method: "POST",
+          headers: {
             "Content-Type": "application/json",
-            "authorization": localStorage.getItem("authToken")!
-          }
-        })
-        const data = await res.json();
-        return data;
-  
-      }
-      catch(error: any){
-        console.log(error.message)
-      }
+            authorization: localStorage.getItem("authToken")!,
+          },
+          body: JSON.stringify({ title: noteTitle }),
+        };
+        const res = await fetch(
+          `${BASE_URL}/note/createNotes`,
+          createNoteParameter
+        );
+        if (res.ok) {
+          const data = await res.json();
 
-})
+          const newNote: NoteType = {
+            _id: data.note_id,
+            title: noteTitle,
+            isOpen: false,
+          };
 
-export const createNewNote = createAsyncThunk('note/createNewNote', async({noteTitle, setNoteTitle, addNewNotes}: createNoteProps) => {
-    try{
-
-        if (noteTitle !== ""){
-          
-          const createNoteParameter = {
-            method: 'POST',
-            headers: {
-              "Content-Type": 'application/json',
-              "authorization": localStorage.getItem("authToken")!
-            },
-            body: JSON.stringify({title: noteTitle})
-          }
-          const res = await fetch(`${BASE_URL}/note/createNotes`, createNoteParameter)
-          if (res.ok){
-            const data = await res.json();
-
-            const newNote: NoteType  = {
-              _id: data.note_id,
-              title: noteTitle,
-              isOpen: false
-            }
-
-            addNewNotes(newNote);
-            setNoteTitle('')
-
-          }
+          thunkAPI.dispatch(addNewNotes(newNote));
+          thunkAPI.dispatch(setNoteTitle(""));
         }
-       
       }
-      catch(error: any){
-        console.log(error.message)
-      }
-})
-
+    } catch (error: any) {
+      console.log(error.message);
+      return thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 export const noteSlice = createSlice({
-    name: 'notes',
-    initialState,
-    reducers: {
-        addNewNotes: (state, action) => {
-            console.log('This is new note: ', action.payload)
-            state.notes.push = action.payload            
-        },
-
-        setNoteTitle: (state, action): void => {
-            state.noteTitle = action.payload;
-        }
-
+  name: "notes",
+  initialState,
+  reducers: {
+    addNewNotes: (state, action) => {
+      state.notes.push(action.payload);
     },
-    extraReducers: (builder) => {
-        builder
-            .addCase(fetchAllNotes.pending, () => {
-                console.log('pending status')
-            })
-            .addCase(fetchAllNotes.fulfilled, (state, action) => {
-                const {messageType} = action.payload;
-                if (messageType === 'success'){
-                    const notes = action.payload.notes
-                    console.log(notes)
-                    state.notes = notes;
-                }
-                else {
-                    console.log("Error: ", action.payload)
-                }
 
-            })
-            .addCase(fetchAllNotes.rejected, () => {
-                console.log('Rejected  status')
-            })
-    }
-})
+    setNoteTitle: (state, action): void => {
+      state.noteTitle = action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(fetchAllNotes.pending, (_, action) => {
+      console.log(action.payload);
+    });
+    builder.addCase(fetchAllNotes.fulfilled, (state, action) => {
+      const { messageType } = action.payload;
+      if (messageType === "success") {
+        const notes = action.payload.notes;
+        console.log(notes);
+        state.notes = notes;
+      } else {
+        console.log("Error: ", action.payload);
+      }
+    });
+
+    builder.addCase(fetchAllNotes.rejected, (state, action) => {
+      state.errors = action.payload;
+    });
+  },
+});
 
 export default noteSlice.reducer;
 export const { setNoteTitle, addNewNotes } = noteSlice.actions;
--- src/pages/NotePage.tsx
+++ src/pages/NotePage.tsx
@@ -1,209 +1,210 @@
-import { Note } from '../@components/Note';
+import { Note } from "../@components/Note";
 import { TabsBar } from "../@components/TabsBar";
 import { Editor } from "../@components/Editor";
 import { useEffect, useState } from "react";
-import { useNotes } from '../hooks/useNotes';
-import { useSaveContent } from '../hooks/useSaveContent';
-import { useTab } from '../hooks/useTab';
-import { getNextTab, NoteType } from '../utils';
-import { fetchAllNotes, createNewNote, setNoteTitle, addNewNotes } from '../features/NoteSlice';
-import { useDispatch, useSelector } from 'react-redux';
-import { AppDispatch, RootState } from '../app/store';
+import { useNotes } from "../hooks/useNotes";
+import { useSaveContent } from "../hooks/useSaveContent";
+import { useTab } from "../hooks/useTab";
+import { getNextTab, NoteType } from "../utils";
+import {
+  fetchAllNotes,
+  createNewNote,
+  setNoteTitle,
+  addNewNotes,
+} from "../features/NoteSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../app/store";
 
- 
-export interface TabsType  extends NoteType{
+export interface TabsType extends NoteType {
   selectedTab: boolean;
-  content: string ;
-  noteId: string
+  content: string;
+  noteId: string;
 }
 
-
 export const NotePage = () => {
-
   const dispatch: AppDispatch = useDispatch();
-  const { notes, noteTitle } = useSelector((state: RootState) => state.notes) 
-  console.log('this is notes; ', notes)
+  const { notes, noteTitle } = useSelector((state: RootState) => state.notes);
+  console.log("this is notes; ", notes);
 
-  const {  setNotes } = useNotes();  
-  // const[noteTitle, setNoteTitle] = useState<string>('');
-  console.log("rendor")
-  console.log("this is note title: ", noteTitle)
-  
-  
-  const [ selectedTab, setSelectedTab ] = useState<TabsType>({
+  const { setNotes } = useNotes();
+
+  console.log("rendor");
+  console.log("this is note title: ", noteTitle);
+
+  const [selectedTab, setSelectedTab] = useState<TabsType>({
     _id: "",
     title: "",
     selectedTab: false,
     content: "",
-    noteId: ""
+    noteId: "",
   });
 
   const [previousId, setPreviousId] = useState<string>("");
-  
+
   const { saveContent } = useSaveContent(previousId);
 
-  const { tabs, setTabs , getAllTabs, openNewTab, selectNextTab, removeTab}  = useTab({
-    setSelectedTab,
-    notes,
-    setNotes,
-    saveContent,
-    previousId
-  });
-  
-  useEffect(() => {
-    dispatch(fetchAllNotes())
-    getAllTabs()
-  }, [])
+  const { tabs, setTabs, getAllTabs, openNewTab, selectNextTab, removeTab } =
+    useTab({
+      setSelectedTab,
+      notes,
+      setNotes,
+      saveContent,
+      previousId,
+    });
 
-  
-  
-  const deleteNote = async(noteId: string) => {
-    try{
-      const res = await fetch('http://localhost:8000/note/delete-note', {
+  useEffect(() => {
+    getAllTabs();
+  }, []);
+
+  useEffect(() => {
+    console.log('use effect fire')
+    dispatch(fetchAllNotes());
+
+  }, [dispatch, addNewNotes]);
+
+  const deleteNote = async (noteId: string) => {
+    try {
+      const res = await fetch("http://localhost:8000/note/delete-note", {
         method: "DELETE",
         headers: {
           "Content-Type": "application/json",
-          "authorization": localStorage.getItem("authToken")!
+          authorization: localStorage.getItem("authToken")!,
         },
-        body: JSON.stringify({noteId: noteId})
-      })
-      if (res.ok){
-
+        body: JSON.stringify({ noteId: noteId }),
+      });
+      if (res.ok) {
         // 1. first update the notes array locally
-        const remainingNote = notes.filter(note => note._id !== noteId);
+        const remainingNote = notes.filter((note) => note._id !== noteId);
         setNotes(remainingNote);
 
-      
         // 2. get the next tab to select
-        const removedTab = tabs.find(tab => tab.noteId === noteId);
+        const removedTab = tabs.find((tab) => tab.noteId === noteId);
 
-        if (removedTab){
+        if (removedTab) {
           const nextTab = getNextTab(tabs, removedTab._id);
-          if (nextTab._id === ''){
+          if (nextTab._id === "") {
             setSelectedTab(nextTab);
-          }
-          else {
+          } else {
             selectNextTab(nextTab, previousId);
           }
         }
         // 3. update the tabs array (remove the tab with noteId === above noteId)
-        const remainingTabs = tabs.filter(tab => tab.noteId !== noteId);
+        const remainingTabs = tabs.filter((tab) => tab.noteId !== noteId);
         setTabs(remainingTabs);
-
       }
-
+    } catch (error: any) {
+      console.error(error.message);
     }
-    catch(error: any){
-      console.error(error.message)
-    }
-  }
+  };
 
-
-  const editNoteTitle = async(noteId: string, newTitle: string) =>  {
-
+  const editNoteTitle = async (noteId: string, newTitle: string) => {
     // check if the title with noteId and newTitle are different then only make a backend call otherwise don't
-    const note  = notes.find(note => note._id === noteId)
-    if (note && note.title !== newTitle){
+    const note = notes.find((note) => note._id === noteId);
+    if (note && note.title !== newTitle) {
       try {
-        const res = await fetch('http://localhost:8000/note/update-note-title', {
-          method: 'POST',
-          headers: {
-            "Content-Type": "application/json",
-            "authorization": localStorage.getItem("authToken")!
-          },
-          body: JSON.stringify({
-            noteId: noteId,
-            newTitle: newTitle
-          })            
-        })
-  
-        if (res.ok){
+        const res = await fetch(
+          "http://localhost:8000/note/update-note-title",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              authorization: localStorage.getItem("authToken")!,
+            },
+            body: JSON.stringify({
+              noteId: noteId,
+              newTitle: newTitle,
+            }),
+          }
+        );
+
+        if (res.ok) {
           // 1. update the title from note array
-          const updatedNoteArray = notes.map(note => note._id === noteId ? {...note, title: newTitle} : note);
+          const updatedNoteArray = notes.map((note) =>
+            note._id === noteId ? { ...note, title: newTitle } : note
+          );
           setNotes(updatedNoteArray);
-  
+
           // 2. update the title from tab array
-          const updatedTabsArray = tabs.map(tab => tab.noteId === noteId ? {...tab, title: newTitle}: tab);
+          const updatedTabsArray = tabs.map((tab) =>
+            tab.noteId === noteId ? { ...tab, title: newTitle } : tab
+          );
           setTabs(updatedTabsArray);
         }
-      }
-      catch(error: any) {
+      } catch (error: any) {
         console.log(error.message);
-  
       }
-
     }
-  }
-  
+  };
 
   return (
     <>
       <main className="max-h-screen text-white flex ">
         <section className="flex w-60  fixed top-0 bottom-0 flex-col bg-gray-800">
-            <h1 className="text-3xl flex  justify-center ">My Notes</h1>
-            <div className="flex justify-center flex-col">
-              <div className="flex  justify-between  ">
-                <input 
-                  type="text"
-                  value=  {noteTitle}
-                  className="px-2 py-1 mt-2 text-black w-[75%] outline-none     rounded-sm"
-                  placeholder="Note..."
-                  onChange={(e) => setNoteTitle(e.target.value)}
-                  onKeyDown={(e) => {
-                    if(e.code === 'Enter'){
-                      dispatch(createNewNote({noteTitle, setNoteTitle, addNewNotes}))
-                    }
-                  }}
-
-                  />
-                <button 
-                  className="bg-[#15202B] mt-2 w-14 font-semibold border border-white rounded-md  shadow-lg shadow-cyan-500/50 "
-                  onClick={() => dispatch(createNewNote({noteTitle, setNoteTitle, addNewNotes}))}
-                  >
-                    Add
-                  </button>
-
-              </div>
-              <div className="flex flex-col gap-2 mt-2 ">
-                {
-                  notes.map((note, index) => {
-                    return <Note 
-                      id = {note._id}
-                      key = {index}
-                      title = {note.title}
-                      isOpen = {note.isOpen!}
-                      openNewTab = {openNewTab}
-                      deleteNote={deleteNote}
-                      onUpdate = {editNoteTitle}
-                    /> 
-                  })
+          <h1 className="text-3xl flex  justify-center ">My Notes</h1>
+          <div className="flex justify-center flex-col">
+            <div className="flex  justify-between  ">
+              <input
+                type="text"
+                value={noteTitle}
+                className="px-2 py-1 mt-2 text-black w-[75%] outline-none     rounded-sm"
+                placeholder="Note..."
+                onChange={(e) => dispatch(setNoteTitle(e.target.value))}
+                onKeyDown={(e) => {
+                  if (e.code === "Enter") {
+                    dispatch(
+                      createNewNote({ noteTitle, setNoteTitle, addNewNotes })
+                    );
+                  }
+                }}
+              />
+              <button
+                className="bg-[#15202B] mt-2 w-14 font-semibold border border-white rounded-md  shadow-lg shadow-cyan-500/50 "
+                onClick={() =>
+                  dispatch(
+                    createNewNote({ noteTitle, setNoteTitle, addNewNotes })
+                  )
                 }
+              >
+                Add
+              </button>
             </div>
-
-
+            <div className="flex flex-col gap-2 mt-2 ">
+              {notes.map((note, index) => {
+                return (
+                  <Note
+                    id={note._id}
+                    key={index}
+                    title={note.title}
+                    isOpen={note.isOpen!}
+                    openNewTab={openNewTab}
+                    deleteNote={deleteNote}
+                    onUpdate={editNoteTitle}
+                  />
+                );
+              })}
+            </div>
           </div>
-        </section >
+        </section>
 
-        <section className = 'fixed top-0 bottom-0 left-60 right-0 flex flex-col bg-gray-900' >
-          <TabsBar 
-            tabs = {tabs}
+        <section className="fixed top-0 bottom-0 left-60 right-0 flex flex-col bg-gray-900">
+          <TabsBar
+            tabs={tabs}
             selectedTab={selectedTab}
             selectNextTab={selectNextTab}
-            removeTab = {removeTab}
-            previousId = {previousId}
+            removeTab={removeTab}
+            previousId={previousId}
           />
           <div className="text-white px-4 py-2 fixed top-10 bottom-0 left-60 right-0">
-            <Editor 
-              selectedTab = { selectedTab }
-              setSelectedTab = {setSelectedTab}
+            <Editor
+              selectedTab={selectedTab}
+              setSelectedTab={setSelectedTab}
               tabs={tabs}
               setTabs={setTabs}
-              setPreviousId = {setPreviousId}
-            /> 
+              setPreviousId={setPreviousId}
+            />
           </div>
         </section>
       </main>
     </>
-  )
-
-}
+  );
+};
